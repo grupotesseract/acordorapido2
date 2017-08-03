1b083aecf18c4b0f048c39b605fb203d74af4706
Renato Gomes <renatoo.agomes@gmail.com>
2017-08-02 23:27:46 -0300

    Commit com Seeder quase lá

--- database/seeds/EsqueletoSeeder.php
+++ database/seeds/EsqueletoSeeder.php
@@ -15,32 +15,33 @@ class EsqueletoSeeder extends Seeder
     public function run()
     {
         try {
-
             // Criando usuário para aluno, escola e admin
-            $userAluno = new App\User();
-            $userAluno->name = 'Evandro Carreira';
-            $userAluno->email = 'evandro.carreira@gmail.com';
-            $userAluno->password = bcrypt(env('ADMIN_PWD', '123321'));
-            $userAluno->save();
-            $userEscola = new App\User();
-            $userEscola->name = 'Escola Teste';
-            $userEscola->email = 'toledo@ite.edu.br';
-            $userEscola->password = bcrypt(env('ADMIN_PWD', '123321'));
-            $userEscola->save();
-            $userAdmin = new App\User();
-            $userAdmin->name = 'Edilson Alexandre';
-            $userAdmin->email = 'edilson.bauru@gmail.com';
-            $userAdmin->password = bcrypt(env('ADMIN_PWD', '123321'));
-            $userAdmin->save();
+            $userAluno = factory(App\Models\User::class)->create([
+                'name' => 'Evandro Carreira',
+                'email' => 'evandro.carreira@gmail.com',                     
+                'password' => bcrypt(env('ADMIN_PWD', '123321'))
+            ]);
+
+            $userEscola = factory(App\Models\User::class)->create([
+                'name' => 'Escola Teste',
+                'email' => 'toledo@ite.edu.br',                     
+                'password' => bcrypt(env('ADMIN_PWD', '123321'))
+            ]);
+
+            $userAdmin = factory(App\Models\User::class)->create([
+                'name' => 'Edilson Alexandre',
+                'email' => 'edilson.bauru@gmail.com',                     
+                'password' => bcrypt(env('ADMIN_PWD', '123321'))
+            ]);
 
             // Criando entidades de aluno e escola e associando o user
-            $c = new App\Cliente();
+            $c = new App\Models\Cliente();
             $c->nome = 'Evandro Barbosa Carreira';
             $c->rg = '46.755.061-2';
             $c->user()->associate($userAluno);
             $c->save();
 
-            $e = new App\Empresa();
+            $e = new App\Models\Empresa();
             $e->nome = 'EscolaTeste';
             $e->cidade = 'Bauru';
             $e->estado = 'SP';
@@ -48,19 +49,19 @@ class EsqueletoSeeder extends Seeder
             $e->save();
 
             // Criando funções dentro do sistema
-            $escola = new App\Role();
+            $escola = new App\Models\Role();
             $escola->name = 'escola';
             $escola->display_name = 'Instituição de ensino';
             $escola->description = 'Usuário autorizado a visualizar os dados referentes à sua escola';
             $escola->save();
 
-            $admin = new App\Role();
+            $admin = new App\Models\Role();
             $admin->name = 'admin';
             $admin->display_name = 'Admin do sistema';
             $admin->description = 'Usuário autorizado a excluir, editar e incluir alunos, escolas e novos usuários';
             $admin->save();
             
-            $aluno = new App\Role();
+            $aluno = new App\Models\Role();
             $aluno->name = 'aluno';
             $aluno->display_name = 'Usuário Aluno';
             $aluno->description = 'Usuário autorizado a visualizar somente seus títulos e infos pessoais';
@@ -70,9 +71,14 @@ class EsqueletoSeeder extends Seeder
             $userAluno->attachRole($aluno);
             $userEscola->attachRole($escola);
             $userAdmin->attachRole($admin);
+
         } catch (\Illuminate\Database\QueryException $exception) {
-            dd($exception);
+            dd($exception->getMessage());
             echo 'erro';
         }
+    }
+
+    private function seedUsers() 
+    {
     }
 }
