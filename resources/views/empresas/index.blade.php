3c72d279735452f45aad89763addeb97f67ccb14
Evandro Carreira <evandro.carreira@gmail.com>
2017-08-22 22:36:21 -0300

    clientes > alunos; empresas > escolas

--- resources/views/empresas/index.blade.php
+++ resources/views/empresas/index.blade.php
@@ -4,7 +4,7 @@
     <section class="content-header">
         <h1 class="pull-left">Empresas</h1>
         <h1 class="pull-right">
-           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('empresas.create') !!}">Incluir</a>
+           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('escolas.create') !!}">Incluir</a>
         </h1>
     </section>
     <div class="content">
