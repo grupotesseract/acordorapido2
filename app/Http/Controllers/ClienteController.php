971a64016173c44f76b415eaec71986601a1d85e
Evandro Carreira <evandro.carreira@gmail.com>
2017-08-24 20:42:57 -0300

    fix de rota de clientes

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -55,7 +55,7 @@ class ClienteController extends AppBaseController
 
         Flash::success('Cliente salvo com sucesso.');
 
-        return redirect(route('clientes.index'));
+        return redirect(route('alunos.index'));
     }
 
     /**
@@ -72,8 +72,10 @@ class ClienteController extends AppBaseController
         if (empty($cliente)) {
             Flash::error('Cliente não encontrado');
 
-            return redirect(route('clientes.index'));
+            return redirect(route('alunos.index'));
         }
+
+        $cliente->titulos; 
 
         if($cliente->celular) {
             $cliente->contato = $cliente->celular;
@@ -104,7 +106,7 @@ class ClienteController extends AppBaseController
         if (empty($cliente)) {
             Flash::error('Cliente não encontrado');
 
-            return redirect(route('clientes.index'));
+            return redirect(route('alunos.index'));
         }
 
         return view('clientes.edit')->with('cliente', $cliente);
@@ -125,14 +127,14 @@ class ClienteController extends AppBaseController
         if (empty($cliente)) {
             Flash::error('Cliente não encontrado');
 
-            return redirect(route('clientes.index'));
+            return redirect(route('alunos.index'));
         }
 
         $cliente = $this->clienteRepository->update($request->all(), $id);
 
         Flash::success('Cliente atualizado com sucesso.');
 
-        return redirect(route('clientes.index'));
+        return redirect(route('alunos.index'));
     }
 
     /**
@@ -149,13 +151,13 @@ class ClienteController extends AppBaseController
         if (empty($cliente)) {
             Flash::error('Cliente não encontrado');
 
-            return redirect(route('clientes.index'));
+            return redirect(route('alunos.index'));
         }
 
         $this->clienteRepository->delete($id);
 
         Flash::success('Cliente excluído com sucesso.');
 
-        return redirect(route('clientes.index'));
+        return redirect(route('alunos.index'));
     }
 }
--- resources/views/clientes/fields.blade.php
+++ resources/views/clientes/fields.blade.php
@@ -61,5 +61,5 @@
 <!-- Submit Field -->
 <div class="form-group col-sm-12">
     {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-    <a href="{!! route('clientes.index') !!}" class="btn btn-default">Cancel</a>
+    <a href="{!! route('alunos.index') !!}" class="btn btn-default">Cancel</a>
 </div>
