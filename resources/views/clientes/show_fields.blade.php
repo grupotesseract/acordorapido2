19d7c1ecbaa54d75101058b0d27eea8f47ce6378
Evandro Carreira <evandro.carreira@gmail.com>
2017-08-24 17:28:09 -0300

    View de aluno

--- resources/views/clientes/show_fields.blade.php
+++ resources/views/clientes/show_fields.blade.php
@@ -1,31 +1,13 @@
-<!-- Id Field -->
-<div class="form-group">
-    {!! Form::label('id', 'Id:') !!}
-    <p>{!! $cliente->id !!}</p>
-</div>
-
 <!-- Nome Field -->
 <div class="form-group">
     {!! Form::label('nome', 'Nome:') !!}
     <p>{!! $cliente->nome !!}</p>
 </div>
 
-<!-- User Id Field -->
+<!-- Rg Field -->
 <div class="form-group">
-    {!! Form::label('user_id', 'User Id:') !!}
-    <p>{!! $cliente->user_id !!}</p>
-</div>
-
-<!-- Created At Field -->
-<div class="form-group">
-    {!! Form::label('created_at', 'Created At:') !!}
-    <p>{!! $cliente->created_at !!}</p>
-</div>
-
-<!-- Updated At Field -->
-<div class="form-group">
-    {!! Form::label('updated_at', 'Updated At:') !!}
-    <p>{!! $cliente->updated_at !!}</p>
+    {!! Form::label('rg', 'Rg:') !!}
+    <p>{!! $cliente->rg !!}</p>
 </div>
 
 <!-- Turma Field -->
@@ -36,43 +18,51 @@
 
 <!-- Periodo Field -->
 <div class="form-group">
-    {!! Form::label('periodo', 'Periodo:') !!}
+    {!! Form::label('periodo', 'Período:') !!}
     <p>{!! $cliente->periodo !!}</p>
 </div>
 
 <!-- Responsavel Field -->
 <div class="form-group">
-    {!! Form::label('responsavel', 'Responsavel:') !!}
+    {!! Form::label('responsavel', 'Responsável:') !!}
     <p>{!! $cliente->responsavel !!}</p>
 </div>
 
-<!-- Celular Field -->
-<div class="form-group">
-    {!! Form::label('celular', 'Celular:') !!}
-    <p>{!! $cliente->celular !!}</p>
+<div class="row">
+    <!-- Celular Field -->
+    <div class="form-group col-sm-3">
+        {!! Form::label('celular', 'Celular:') !!}
+        <p>{!! $cliente->celular !!}</p>
+    </div>
+
+    @if( $cliente->celular2)
+    <!-- Celular2 Field -->
+    <div class="form-group col-sm-3">
+        {!! Form::label('celular2', 'Celular2:') !!}
+        <p>{!! $cliente->celular2 !!}</p>
+    </div>
+    @endif
 </div>
 
-<!-- Telefone Field -->
-<div class="form-group">
-    {!! Form::label('telefone', 'Telefone:') !!}
-    <p>{!! $cliente->telefone !!}</p>
+<div class="row">
+    <!-- Telefone Field -->
+    <div class="form-group col-sm-3">
+        {!! Form::label('telefone', 'Telefone:') !!}
+        <p>{!! $cliente->telefone !!}</p>
+    </div>
+
+    @if( $cliente->telefone2)
+    <!-- Telefone2 Field -->
+    <div class="form-group col-sm-3">
+        {!! Form::label('telefone2', 'Telefone2:') !!}
+        <p>{!! $cliente->telefone2 !!}</p>
+    </div>
+    @endif
 </div>
 
-<!-- Telefone2 Field -->
+<!-- Created At Field -->
 <div class="form-group">
-    {!! Form::label('telefone2', 'Telefone2:') !!}
-    <p>{!! $cliente->telefone2 !!}</p>
-</div>
-
-<!-- Celular2 Field -->
-<div class="form-group">
-    {!! Form::label('celular2', 'Celular2:') !!}
-    <p>{!! $cliente->celular2 !!}</p>
-</div>
-
-<!-- Rg Field -->
-<div class="form-group">
-    {!! Form::label('rg', 'Rg:') !!}
-    <p>{!! $cliente->rg !!}</p>
+    {!! Form::label('created_at', 'Criado em:') !!}
+    <p>{!! $cliente->created_at !!}</p>
 </div>
 
