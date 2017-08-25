6d8bca3adac01ab0f07a1b47a0de7c607d2345f0
Fernando Fernandes <fvlimafernandes@gmail.com>
2017-08-25 04:19:16 +0000

    MouseOver no ícone de operação efetuada, mostra informação de quem enviou e que horas

--- resources/views/titulos/operacoes.blade.php
+++ resources/views/titulos/operacoes.blade.php
@@ -1,35 +1,35 @@
 @foreach ($avisos as $aviso)
-    @foreach ($aviso['avisosenviados'] as $avisoenviado)
+  @foreach ($aviso['avisosenviados'] as $avisoenviado)
 
-        <?php 
-          switch ($aviso['estado']) {
-            case 'azul':
-              $bootStrapClass = 'primary';
-              break;
-            case 'verde':
-              $bootStrapClass = 'success';
-              break;
-            case 'amarelo':
-              $bootStrapClass = 'warning';
-              break;
-            case 'vermelho':
-              $bootStrapClass = 'danger';
-              break;
-          }
+      <?php 
+        switch ($aviso['estado']) {
+          case 'azul':
+            $bootStrapClass = 'primary';
+            break;
+          case 'verde':
+            $bootStrapClass = 'success';
+            break;
+          case 'amarelo':
+            $bootStrapClass = 'warning';
+            break;
+          case 'vermelho':
+            $bootStrapClass = 'danger';
+            break;
+        }
 
-          switch ($avisoenviado['tipodeaviso']) {
-            case 0:
-              $iconClass = 'glyphicon glyphicon-envelope';
-              break;
-            case 1:
-              $iconClass = 'glyphicon glyphicon-earphone';
-              break;
-          }    
-        ?>
+        switch ($avisoenviado['tipodeaviso']) {
+          case 0:
+            $iconClass = 'glyphicon glyphicon-envelope';
+            break;
+          case 1:
+            $iconClass = 'glyphicon glyphicon-earphone';
+            break;
+        }    
+      ?>
 
-        <span class="label label-{{ $bootStrapClass }}"><span class="{{ $iconClass }}" aria-hidden="true"></span></span>
+      <span class="label label-{{ $bootStrapClass }}" data-toggle="tooltip" title="Enviado por {{$avisoenviado['user']['name']}} em {{\Carbon\Carbon::parse($avisoenviado['created_at'])->format('d/m/Y H:i:s')}}"><span class="{{ $iconClass }}" aria-hidden="true"></span></span>
 
-    @endforeach
+  @endforeach
 
 
 @endforeach
