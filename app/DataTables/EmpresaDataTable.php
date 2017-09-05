712adb974e65d31ffb4cdcb4aa033e890342f5f0
Renato Gomes <renatoo.agomes@gmail.com>
2017-09-05 05:44:54 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/DataTables/EmpresaDataTable.php
+++ app/DataTables/EmpresaDataTable.php
@@ -59,7 +59,7 @@ class EmpresaDataTable extends DataTable
                          'text'    => '<i class="fa fa-download"></i> Exportar',
                          'buttons' => [
                              'csv',
-                             'excel'
+                             'excel',
                          ],
                     ],
                     [
