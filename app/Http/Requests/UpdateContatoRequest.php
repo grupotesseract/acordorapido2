bad7cc0a9da6626f9c9c1c2010ebb9a9b8a42cab
Renato Gomes <renatoo.agomes@gmail.com>
2017-09-22 03:03:01 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Requests/UpdateContatoRequest.php
+++ app/Http/Requests/UpdateContatoRequest.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
 use App\Models\Contato;
+use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateContatoRequest extends FormRequest
 {
-
     /**
      * Determine if the user is authorized to make this request.
      *
