b96d427a24c8198a45c87558a43295f4830b7eed
Renato Gomes <renatoo.agomes@gmail.com>
2017-10-04 02:19:53 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/DataTables/AcordoDataTable.php
+++ app/DataTables/AcordoDataTable.php
@@ -3,12 +3,10 @@
 namespace App\DataTables;
 
 use App\Models\Acordo;
-use Form;
 use Yajra\Datatables\Services\DataTable;
 
 class AcordoDataTable extends DataTable
 {
-
     /**
      * @return \Illuminate\Http\JsonResponse
      */
@@ -59,8 +57,8 @@ class AcordoDataTable extends DataTable
                              'pdf',
                          ],
                     ],
-                    'colvis'
-                ]
+                    'colvis',
+                ],
             ]);
     }
 
@@ -77,7 +75,7 @@ class AcordoDataTable extends DataTable
             'observacao' => ['name' => 'observacao', 'data' => 'observacao'],
             'user_id' => ['name' => 'user_id', 'data' => 'user_id'],
             'cliente_id' => ['name' => 'cliente_id', 'data' => 'cliente_id'],
-            'empresa_id' => ['name' => 'empresa_id', 'data' => 'empresa_id']
+            'empresa_id' => ['name' => 'empresa_id', 'data' => 'empresa_id'],
         ];
     }
 
