254f36e2063445600d25ff59374e9df7bae7d3cd
Fernando Fernandes <fvlimafernandes@gmail.com>
2017-10-21 00:40:21 +0000

    Inserção dos box de info de alunos e escolas

--- resources/views/acordos/create_final.blade.php
+++ resources/views/acordos/create_final.blade.php
@@ -8,10 +8,45 @@
     </section>
     <div class="content">
         @include('adminlte-templates::common.errors')
-        <div class="box box-primary" data-widget="box-widget">
-            
+        
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="box box-solid box-primary" data-widget="box-widget">
+                    <div class="box-header">
+                        <h3 class="box-title">{{$empresa->nome}}</h3>
+                    </div>
+                    <div class="box-body">      
+                        <p>Cidade: {{$empresa->cidade}} </p>
+                        <p>Estado: {{$empresa->estado}}</p>              
+                        <p>Multa após vencimento: {{$empresa->multaporc}}% </p>
+                        <p>Mora (juros diários sobre o valor acumulado): {{$empresa->multadiariaporc}}%</p>
+                    </div>
+
+                </div>          
+
+            </div>
+
+            <div class="col-sm-6">
+                <div class="box box-solid box-primary" data-widget="box-widget">
+                    <div class="box-header">
+                        <h3 class="box-title">{{$aluno->nome}}</h3>
+                    </div>
+                    <div class="box-body">      
+                        <p>Turma: {{$aluno->turma}} </p>
+                        <p>Período: {{$aluno->periodo}}</p>              
+                        <p>Responsável: {{$aluno->responsavel}} </p>
+                        <p>Celular 1: {{$aluno->celular}}</p>
+                        <p>Celular 2: {{$aluno->celular2}}</p>
+                        <p>Telefone 1: {{$aluno->telefone}}</p>
+                        <p>Telefone 2: {{$aluno->telefone2}}</p>
+                    </div>
+
+                </div> 
+            </div>
+        </div>
+
+        <div class="box box-primary">           
             <div class="box-header">
-                <h3 class="box-title">Escola: {{$empresa->nome}}</h3>
                 <h3 class="box-title">Títulos Referentes ao Aluno: {{$aluno->nome}}</h3>
             </div>
 
