e0e180068527e565638343d749227cbd1ffa027e
Evandro Carreira <evandro.carreira@gmail.com>
2017-10-24 21:51:24 -0200

    Permissões atualizadas e ligadas ao menu (sem filtragem no backend por enquanto)

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,45 +1,72 @@
 <!-- Optionally, you can add icons to the links -->
 <li class="{{ Request::is('home*') ? 'active' : '' }}"><a href="{{ url('home') }}"><i class='glyphicon glyphicon-home'></i> <span>{{ trans('message.home') }}</span></a></li>
 
-@role('admin')
+@ability('admin', 'gerenciar-escolas|gerenciar-alunos')
 <li class="{{ Request::is('dashboard*') ? 'active' : '' }}"><a href="{{ url('dashboard') }}"><i class='glyphicon glyphicon-dashboard'></i> <span>Estatísticas</span></a></li>
-<li class="{{ Request::is('escolas*') || Request::is('alunos*') || Request::is('users*') ? 'active' : '' }} treeview">
+<li class="{{ Request::is('escolas*') || Request::is('alunos*') ? 'active' : '' }} treeview">
     <a href="#"><i class='glyphicon glyphicon-th-list'></i> <span>Cadastros</span><i class="fa fa-angle-left pull-right"></i></a>
     <ul class="treeview-menu">
+        @ability('admin', 'gerenciar-escolas|gerenciar-alunos')
         <li class="{{ Request::is('escolas*') ? 'active' : '' }}"><a href="{{ url('escolas') }}"><i class='glyphicon glyphicon-education'></i>Escolas</a></li>
-        <li class="{{ Request::is('alunos*') ? 'active' : '' }}">
-            <a href="{{ url('alunos') }}"><i class='glyphicon glyphicon-user'></i> <span>Alunos</span></a>
-        </li>
-        <li class="{{ Request::is('users*') ? 'active' : '' }} treeview">
-            <a href="#"><i class='glyphicon glyphicon-user'></i><span>Acessos</span><i class="fa fa-angle-left pull-right"></i></a>
-            <ul class="treeview-menu">
-                <li class="{{ Request::is('users*') ? 'active' : '' }}">
-                    <a href="{!! route('users.index') !!}"><i class="fa fa-edit"></i><span>Usuários</span></a>
-                </li>                
-                <li> <a href="#"><i class='glyphicon glyphicon-lock'></i> <span>Permissões</span> </a></li>
-            </ul>
-        </li>
+        @endability
+        @ability('admin', 'gerenciar-escolas|gerenciar-alunos')
+        <li class="{{ Request::is('alunos*') ? 'active' : '' }}"> <a href="{{ url('alunos') }}"><i class='glyphicon glyphicon-user'></i> <span>Alunos</span></a> </li>
+        @endability
     </ul>
 </li>
+@endability
+@ability('admin', 'gerenciar-usuarios')
+<li class="{{ Request::is('users*') ? 'active' : '' }} treeview">
+    <a href="#"><i class='glyphicon glyphicon-user'></i><span>Acessos</span><i class="fa fa-angle-left pull-right"></i></a>
+    <ul class="treeview-menu">
+        <li class="{{ Request::is('users*') ? 'active' : '' }}">
+            <a href="{!! route('users.index') !!}"><i class="fa fa-edit"></i><span>Usuários</span></a>
+        </li>                
+    </ul>
+</li>
+@endability
+
+@ability('admin', 'importar-planilhas-*')
 <li class="{{ Request::is('importacao*') ? 'active' : '' }} treeview">
     <a href="#"><i class='glyphicon glyphicon-import'></i> <span>Importações</span> <i class="fa fa-angle-left pull-right"></i></a>
     <ul class="treeview-menu">
+        @ability('admin', 'importar-planilhas-azul')
         <li class="{{ Request::is('importacao/azul') ? 'active' : '' }}"><a href="{{url('importacao/azul')}}"><label class="label label-modulo-azul"> <span aria-hidden="true" class="glyphicon glyphicon-envelope"></span></label> Preventiva</a></li>
+        @endability
+        @ability('admin', 'importar-planilhas-verde')
         <li class="{{ Request::is('importacao/verde') ? 'active' : '' }}"><a href="{{url('importacao/verde')}}"><label class="label label-modulo-verde"> <span aria-hidden="true" class="glyphicon glyphicon-earphone"></span></label> Recuperação</a></li>
+        @endability
+        @ability('admin', 'importar-planilhas-amarelo')
         <li class="{{ Request::is('importacao/amarelo') ? 'active' : '' }}"><a href="{{url('importacao/amarelo')}}"><label class="label label-modulo-amarelo"> <span aria-hidden="true" class="glyphicon glyphicon-phone-alt"></span></label> Intensivo</a></li>
+        @endability
+        @ability('admin', 'importar-planilhas-vermelho')
         <li class="{{ Request::is('importacao/vermelho') ? 'active' : '' }}"><a href="{{url('importacao/vermelho')}}"><label class="label label-modulo-vermelho"> <span aria-hidden="true" class="glyphicon glyphicon-alert"></span></label> Cobrança</a></li>
+        @endability
     </ul>
 </li>
+@endability
+
+@ability('admin', 'visualizar-modulo-verde|visualizar-modulo-azul|visualizar-modulo-amarelo|visualizar-modulo-vermelho')
 <li class="{{ Request::is('titulos*') ? 'active' : '' }} treeview">
     <a href="#"><i class='glyphicon glyphicon-th-large'></i> <span>Módulos</span> <i class="fa fa-angle-left pull-right"></i></a>
     <ul class="treeview-menu">
+        @ability('admin', 'visualizar-modulo-azul')
         <li><a href="{{url('titulos/modulo/azul')}}"><label class="label label-modulo-azul"> <span aria-hidden="true" class="glyphicon glyphicon-envelope"></span></label> Prevenção</a></li>
+        @endability
+        @ability('admin', 'visualizar-modulo-verde')
         <li><a href="{{url('titulos/modulo/verde')}}"><label class="label label-modulo-verde"> <span aria-hidden="true" class="glyphicon glyphicon-earphone"></span></label> Recuperação</a></li>
+        @endability
+        @ability('admin', 'visualizar-modulo-amarelo')
         <li><a href="{{url('titulos/modulo/amarelo')}}"><label class="label label-modulo-amarelo"> <span aria-hidden="true" class="glyphicon glyphicon-phone-alt"></span></label> Intensivo</a></li>
+        @endability
+        @ability('admin', 'visualizar-modulo-vermelho')
         <li><a href="{{url('titulos/modulo/vermelho')}}"><label class="label label-modulo-vermelho"> <span aria-hidden="true" class="glyphicon glyphicon-alert"></span></label> Cobrança</a></li>
+        @endability
 
     </ul>
 </li>
+@endability
+@ability('admin', 'enviar-sms')
 <li class="{{ Request::is('avisos*') ? 'active' : '' }} treeview">
     <a href="#"><i class='glyphicon glyphicon-comment'></i> <span>Disparos</span><i class="fa fa-angle-left pull-right"></i></a>
     <ul class="treeview-menu">
@@ -53,7 +80,7 @@
         <li><a href="{{ url('avisos/create') }}"><span aria-hidden="true" class="glyphicon glyphicon-envelope"></span>Enviar SMS para um Número</a></li>
     </ul>
 </li>
-@endrole
+@endability
 <!-- <li>
     <a href="{{ url('titulos') }}"><i class='glyphicon glyphicon-list-alt'></i> <span>Títulos</span></a>
     </li> -->
@@ -79,13 +106,15 @@
         <a href="{!! route('modeloAvisos.index') !!}"><i class="fa fa-edit"></i><span>ModeloAvisos</span></a>
     </li> -->
 
+@ability('admin', 'visualizar-contatos')
 <li class="{{ Request::is('contatos*') ? 'active' : '' }}">
     <a href="{!! route('contatos.index') !!}"><i class="fa fa-edit"></i><span>Contatos - Formulário Site</span></a>
 </li>
+@endability
 
-
-
+@ability('admin', 'criar-acordo')
 <li class="{{ Request::is('acordos*') ? 'active' : '' }}">
     <a href="{!! route('acordos.index') !!}"><i class="fa fa-edit"></i><span>Acordos</span></a>
 </li>
+@endability
 
