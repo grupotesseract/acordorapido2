d2fdf7726bece587de234b49a8af0db6c620b5da
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2018-02-07 17:33:22 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/AcordoController.php
+++ app/Http/Controllers/AcordoController.php
@@ -137,8 +137,7 @@ class AcordoController extends AppBaseController
         $acordo = $this->acordoRepository->create($camposAcordo);
 
         foreach ($input['parcela'] as $key => $valor) {
-            if (!empty($valor) && !empty($input['data'][$key])) {
-
+            if (! empty($valor) && ! empty($input['data'][$key])) {
                 $parcela = $this->parcelamentoRepository->create([
                     'numparcela' => $valor,
                     'dataparcela' => Carbon::createFromFormat('d/m/Y', $input['data'][$key]),
