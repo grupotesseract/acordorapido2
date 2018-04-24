2059af4fb93fae211cd9c53bd8c4c8147c61c62f
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2018-04-24 09:54:06 -0300

    Hotfix com soft delete

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,11 +5,14 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Laratrust\Traits\LaratrustUserTrait;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class User extends Authenticatable
 {
     use LaratrustUserTrait;
     use Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that should be treated as \Carbon\Carbon instances.
