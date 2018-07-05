78a495aa2746f9f626163a929cb952f2233ea7a3
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2018-07-05 10:56:53 -0300

    Horse: withTrashed nos relationships de users

--- app/Models/Historico.php
+++ app/Models/Historico.php
@@ -50,6 +50,6 @@ class Historico extends Model
      */
     public function user()
     {
-        return $this->belongsTo('App\Models\User');
+        return $this->belongsTo('App\Models\User')->withTrashed();
     }
 }
